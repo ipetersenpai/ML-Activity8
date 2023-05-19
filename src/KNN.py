aa66092b5d96a6ee65a9d5e357e3c97753a7528f
Peter Francis Robante <robantepeterfran@gmail.com>
2023-05-19 16:36:38 +0800

    refactor: added the precision,recall,specificity and f1 in the KNN code

--- src/KNN.py
+++ src/KNN.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
 from sklearn.preprocessing import LabelEncoder
 
 dataset = pd.read_csv("IRIS_dataset.csv")
@@ -10,10 +10,8 @@ dataset = pd.read_csv("IRIS_dataset.csv")
 X = dataset.drop('species', axis=1)
 y = dataset['species']
 
-
 label_encoder = LabelEncoder()
 y_encoded = label_encoder.fit_transform(y)
-
 
 X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2)
 
@@ -27,10 +25,17 @@ y_pred = knn.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print('Accuracy:', accuracy)
 
-
 cm = confusion_matrix(y_test, y_pred)
 print('Confusion Matrix:')
 print(cm)
+
+precision = precision_score(y_test, y_pred, average='macro')
+recall = recall_score(y_test, y_pred, average='macro')
+f1 = f1_score(y_test, y_pred, average='macro')
+
+print('Precision:', precision)
+print('Recall:', recall)
+print('F1 Score:', f1)
 
 plt.scatter(X['sepal_length'], X['sepal_width'], c=y_encoded, cmap='viridis')
 plt.scatter(X_test['sepal_length'], X_test['sepal_width'], c=y_pred, cmap='viridis', marker='x')
